039264c06980ee06fc179664ffc412f57bce9439
Nicolás Carmona <carmonanicolas74@gmail.com>
2023-11-11 15:04:45 -0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+const app = express();
 
+// CONNECTION TO MONGODB
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.error(`connection error: ${err}`);
+  });
+
+// RUN SERVER
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
