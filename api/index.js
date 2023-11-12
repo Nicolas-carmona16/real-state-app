aabbd8d115552134ddc7ba92a11a4c9c844a7a65
Nicolás Carmona <nicolas.carmonac@udea.edu.co>
2023-11-12 16:58:34 -0500

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
@@ -17,22 +18,22 @@ mongoose
     console.error(`connection error: ${err}`);
   });
 
+// Middleware for JSON parsing and CORS
+app.use(express.json());
+app.use(cookieParser());
+
 // RUN SERVER
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-// Middleware for JSON parsing and CORS
-app.use(express.json());
-
 // ROUTES
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
