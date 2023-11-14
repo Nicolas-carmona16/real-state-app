4700b8cb8b905ac6fb5761f1404cb9b3452d823e
Nicolás Carmona <nicolas.carmonac@udea.edu.co>
2023-11-13 23:57:06 -0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-blue-800">
+        Discover the Excellence of Real Estate App
+      </h1>
+      <p className="mb-4 text-slate-800">
+        Welcome to Real Estate App, your premier destination for all your real
+        estate needs. As a distinguished real estate agency, we excel in
+        facilitating seamless transactions for buying, selling, and renting
+        properties in the most coveted neighborhoods.
+      </p>
+      <p className="mb-4 text-slate-800">
+        At Real Estate App, we are not just a real estate agency; we are your
+        trusted partners in realizing your property dreams. Our dedicated team
+        of experienced agents is committed to delivering unparalleled service,
+        ensuring that your journey through the intricate process of real estate
+        is not only successful but also enjoyable.
+      </p>
+      <p className="mb-4 text-slate-800">
+        Our mission extends beyond mere transactions; it is about empowering you
+        to achieve your real estate goals. We offer expert advice, personalized
+        service, and an in-depth understanding of the local market dynamics.
+        Whether you're looking to make a property investment, sell your current
+        home, or find the perfect rental, Real Estate App is here to guide you
+        through every step with expertise and dedication.
+      </p>
+      <p className="mb-4 text-slate-800">
+        Real Estate App takes pride in its team of agents, each possessing a
+        wealth of experience and knowledge in the dynamic real estate industry.
+        We go the extra mile to provide the highest level of service, firmly
+        believing that the process of buying or selling a property should be as
+        exciting and rewarding as the outcome itself. Your satisfaction is our
+        priority, and we are devoted to turning your real estate aspirations
+        into reality.
+      </p>
+      <p className="mb-4 text-slate-800">
+        Choose Real Estate App for a real estate experience that transcends
+        expectations. Whether you are a seasoned property investor or a
+        first-time homebuyer, our commitment to excellence remains unwavering.
+        Discover the difference with Real Estate App—where your journey to
+        exceptional real estate begins.
+      </p>
+    </div>
+  );
 }
